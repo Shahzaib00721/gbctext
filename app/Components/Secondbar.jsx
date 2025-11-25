b36fe9a461ee61407ac44bcfa67d8633ae7a74e7
Shahzaib Awan <awan.shahzaib.0314@gmail.com>
2025-11-25 16:56:02 +0500

    update menu

--- app/Components/Secondbar.jsx
+++ app/Components/Secondbar.jsx
@@ -47,17 +47,17 @@ export default function TCFCanada() {
           <div className="text-xs text-gray-500 my-4">
             * Note we are currently only accepting payments through e-transfer or debit at our physical office location in North York.
           </div>
-          <div className="flex flex-col md:flex-row gap-4 mt-8">
+          <div className="flex flex-col md:flex-row gap-4  mt-8">
             <button className="bg-red-500 text-white px-6 py-2 rounded-[6px]  h-[40px] lg:w-[200px] hover:bg-red-600 transition-colors">
               Book Now
             </button>
-            <button className="bg-red-500 text-white px-6 py-3 md:py-0  rounded-[6px]  h-[40px]  lg:w-[423px] hover:bg-red-600 transition-colors">
+            <button className="bg-red-500 text-white px-6 py-2 md:py-0  rounded-[6px]  h-[40px]  lg:w-[423px] hover:bg-red-600 transition-colors">
               Prepare For The Exam
             </button>
           </div>
         </div>
          <div className="space-y-4">
-            {/* Description Paragraphs */}
+          
             <div className="space-y-3">
               <div className="flex gap-3">
                 <div className="w-[13px] h-[13px] bg-[#EF4444] rounded-[2px] flex-shrink-0 mt-1.5"></div>
@@ -81,9 +81,9 @@ export default function TCFCanada() {
               </div>
             </div>
 
-            {/* Logos */}
+         
             <div className="flex items-center gap-8 ">
-              {/* France Education International Logo */}
+         
              
 
            <img src="tch.jpg" alt="" />
